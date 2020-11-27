4e389045cf5f20908f47579a3a21cd7ab9d74e4d
luckyjoker993 <lucky.joker.993@gmail.com>
2020-11-27 06:47:38 +0100

    new way to get links fixed bugs

--- epic_games.py
+++ epic_games.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import traceback
 
 from itertools import repeat
 from time import sleep
@@ -13,7 +14,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 try:
-    from settings import number_of_browsers as workers, save_cookies, hide_browsers
+    from epic_games_settings import number_of_browsers as workers, save_cookies, hide_browsers
 except Exception:
     print('No setting.py file detected, continuing with default settings.')
     workers = 1
@@ -48,24 +49,49 @@ def users():
     return users
 
 
+# def get_links():
+#     options = Options()
+#     if hide_browsers:
+#         options.add_argument('--window-position=-2000,0')
+#     root = webdriver.Chrome(executable_path=path, options=options)
+#     root.get('https://www.epicgames.com/store/en-US/free-games')
+#     WebDriverWait(root, 60).until(lambda wd: len(wd.find_elements_by_xpath(div_discover_xpath)) != 0)
+#     games = root.find_elements_by_xpath(div_discover_xpath)
+#     links = []
+#     # get links of free games
+#     for game in games:
+#         button = game.find_element_by_xpath(text_button_xpath)
+#         if button.text.upper() == 'FREE NOW':
+#             name = game.find_element_by_xpath(name_xpath).text
+#             print(f'{name} is free now')
+#             link = game.find_element_by_xpath(link_xpath)
+#             link = link.get_attribute('href')
+#             links.append((link, 0))
+#     root.close()
+#     return links
+
+
 def get_links():
     options = Options()
     if hide_browsers:
         options.add_argument('--window-position=-2000,0')
     root = webdriver.Chrome(executable_path=path, options=options)
     root.get('https://www.epicgames.com/store/en-US/free-games')
-    WebDriverWait(root, 60).until(lambda wd: len(wd.find_elements_by_xpath(div_discover_xpath)) != 0)
-    games = root.find_elements_by_xpath(div_discover_xpath)
-    links = []
-    # get links of free games
-    for game in games:
-        button = game.find_element_by_xpath(text_button_xpath)
-        if button.text.upper() == 'FREE NOW':
-            name = game.find_element_by_xpath(name_xpath).text
-            print(f'{name} is free now')
-            link = game.find_element_by_xpath(link_xpath)
-            link = link.get_attribute('href')
-            links.append((link, 0))
+    sleep(3)
+    links = zip(set(root.execute_script('''
+        window.scrollTo(0, document.body.scrollHeight);
+        spans = document.getElementsByTagName('span');
+        links = [];
+        for (span of spans) {
+            if(span.innerText.toLowerCase().includes('free now')){
+                while(span.localName != 'a'){
+                    span = span.parentElement;
+                }
+                links.push(span.href);
+            }
+        };
+        return links;
+        ''')), repeat(0))
     root.close()
     return links
 
@@ -145,13 +171,18 @@ def add_games(user, links):
             continue
 
         # if more than one get button add link to repeat
-        [links.append((link, 1)) for _ in range(1, len(get_buttons))]
+        if not repeating:
+            [links.append((link, 1)) for _ in range(1, len(get_buttons))]
+
         # add game to library
         root.execute_script('arguments[0].click()', get_buttons[0])
+        sleep(5)
         success = place_order(root, login)
         if success:
             if not repeating:
                 print(f'{login}: {root.title} added to library')
+            else:
+                print(f'{login}: {root.title} DLC added to library')
             root.execute_script('window.close()')
             root.switch_to.window(root.window_handles[0])
 
@@ -172,32 +203,34 @@ def add_games(user, links):
 
 # function to handle confirming order
 def place_order(root, login):
-    tries = 3
+    tries = 5
     while tries:
         try:
-            WebDriverWait(root, 15).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'btn-primary')))
             root.execute_script("document.getElementsByClassName('btn-primary')[0].click()")
             break
+
         except:
             tries -= 1
             if not tries:
+                traceback.print_exc()
                 print(f'{login}: something went wrong with {root.title}')
                 return False
-    tries = 3
-    while tries:
-        try:
-            WebDriverWait(root, 15).until(
-                EC.presence_of_element_located((By.XPATH, confirm_button_xpath)))
-            root.execute_script(
-                "document.getElementsByClassName('overlay-modal-content')[0]\n"
-                ".getElementsByClassName('btn-primary')[0].click()")
-            break
-        except:
-            tries -= 1
-            if not tries:
-                print(f'{login}: something went wrong with {root.title}')
-                return False
+            sleep(5)
+
+    # Confirm place order # removed
+    # tries = 3
+    # while tries:
+    #     try:
+    #         root.execute_script(
+    #             "document.getElementsByClassName('overlay-modal-content')[0]\n"
+    #             ".getElementsByClassName('btn-primary')[0].click()")
+    #         break
+    #     except:
+    #         tries -= 1
+    #         if not tries:
+    #             traceback.print_exc()
+    #             print(f'{login}: something went wrong with {root.title}')
+    #             return False
 
     sleep(15)
     if '/verify?' in root.current_url:
